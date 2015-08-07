302ba49fd2dc50fa8e09f3fb4ecf11d960c030d4
bcompter <bcompter@gmail.com>
2015-08-06 22:28:10 -0400

    Token look and feel, plus some new images

--- application/models/tokenmodel.php
+++ application/models/tokenmodel.php
@@ -18,8 +18,12 @@ Class Tokenmodel extends MY_Model {
      */
     function get_by_planet($planet_id=0, $location=0)
     {
-        return $this->db->query('SELECT tokens.* FROM tokens '
+        return $this->db->query('SELECT tokens.*, factions.color, formations.type, formations.name AS formation_name'
+                . ' FROM tokens '
                 . 'JOIN planets ON planets.planet_id=tokens.planet_id '
+                . 'JOIN formations ON formations.formation_id=tokens.formation_id '
+                . 'JOIN combat_commands ON combat_commands.command_id=formations.command_id '
+                . 'JOIN factions ON factions.faction_id=combat_commands.faction_id '
                 . 'WHERE tokens.planet_id='.$planet_id.' '
                 . 'AND tokens.location="'.$location.'"')->result();
     }
--- application/views/token_view.php
+++ application/views/token_view.php
@@ -2,6 +2,16 @@
     class="token" 
     action="<?php echo base_url('index.php/token/update_position/'.$token->token_id);  ?>" 
     id="<?php echo $token->token_id ?>"
-    style="top:<?php echo $token->y; ?>px; left:<?php echo $token->x; ?>px">
-    TOKEN
+    style="top:<?php echo $token->y; ?>px; left:<?php echo $token->x; ?>px; background-color:<?php echo $token->color; ?>;">
+    <table>
+        <tr>
+            <?php if($token->detected): ?>
+            <td><div class="inline"><img src="<?php echo base_url('images/mech.png'); ?>"></div></td>
+            <td><div><h1><?php echo substr($token->formation_name, 0, 2); ?></h1></div></td>
+            <?php else: ?>
+            <td><div class="inline"><img src="<?php echo base_url('images/radar.png'); ?>"></div></td>
+            <?php endif; ?>
+        </tr>        
+    </table>
+    
 </div>
