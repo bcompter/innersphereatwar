902aefc59211a1b113e12ceab88f19bc850fe776
bcompter <bcompter@gmail.com>
2015-08-08 23:53:07 -0400

    Nav bar across the top in the template

--- application/views/template.php
+++ application/views/template.php
@@ -21,7 +21,27 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>      
+  <body>
+      
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="home">Inner Sphere at War</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><?php echo anchor('home/dashboard', 'Dashboard'); ?></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+      
       <?php if ($this->session->flashdata('notice') != '') : ?>
         <div class="alert alert-success" role="alert">
           <strong>Notice!</strong> <?php echo $this->session->flashdata('notice'); ?>
